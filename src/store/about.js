9c4352f614e9c85b93cf21475565aadda101071d
sykim0181 <soyeon364@naver.com>
2022-10-03 16:14:46 +0900

    Start Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SOXOY',
+    email: 'soxoy0000@naver.com',
+    blog: 'https://soxoy.blog',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
